73c6a9e8cc8ccecebe0f0b0a284aa33d01442641
Josh Holtrop <jholtrop@gmail.com>
2012-03-10 12:43:31 -0500

    extract SDL version into a variable for easier updating

--- setup.py
+++ setup.py
@@ -4,15 +4,17 @@ import os
 import re
 from distutils.core import setup, Extension
 
+SDL_VERSION = '1.2.14'
+
 def get_sources_from_dir(d):
     return map(lambda x: os.path.join(d, x),
         filter(lambda x: re.search(r'\.cc?$', x), os.listdir(d)))
 
 extra_compile_args = ['-std=gnu99', '-I%s' % os.getcwd()]
 extra_link_args = []
-if os.path.exists('C:\\apps\\SDL-1.2.14'):
-    extra_compile_args.append('-Ic:\\apps\\SDL-1.2.14\\include')
-    extra_link_args.append('-Lc:\\apps\\SDL-1.2.14\\lib')
+if os.path.exists('C:\\apps\\SDL-%s' % SDL_VERSION):
+    extra_compile_args.append('-IC:\\apps\\SDL-%s\\include' % SDL_VERSION)
+    extra_link_args.append('-LC:\\apps\\SDL-%s\\lib' % SDL_VERSION)
 sdl_sources = get_sources_from_dir('sdl')
 sdl_module = Extension('SDL',
         sources = sdl_sources,
